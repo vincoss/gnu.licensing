76ed31dda254a332724028e97af9e813b4a0f37b
Ferdinand Lukasak <feroalien@hotmail.com>
2020-07-15 08:54:54 +1000

    update

--- src/Gnu.Licensing.Sample_XF/Gnu.Licensing.Sample_XF/Services/LicenseService.cs
+++ src/Gnu.Licensing.Sample_XF/Gnu.Licensing.Sample_XF/Services/LicenseService.cs
@@ -10,6 +10,54 @@ using Xamarin.Essentials;
 
 namespace Gnu.Licensing.Sample_XamarinForms.Services
 {
+    public interface IBaseLicense
+    {
+        Task<LicenseResult> RegisterAsync(Guid licenseKey, Guid productId, string url, IDictionary<string, string> attributes);
+        Task<LicenseResult> ValidateAsync();
+    }
+
+    public class LicService
+    {
+        private readonly IBaseLicense _impl;
+
+        public LicService(IBaseLicense impl)
+        {
+            _impl = impl;
+        }
+
+        public Task RunAsync()
+        {
+            var task = Task.Run(async () =>
+            {
+                var result = await _impl.ValidateAsync();
+                if (result.Successful)
+                {
+                    LicenseGlobals.Set(AppLicense.Full);
+                }
+            });
+
+            return task;
+        }
+
+        public Task<LicenseResult> RegisterAsync(Guid licenseKey)
+        {
+            return _impl.RegisterAsync(licenseKey, LicenseGlobals.ProductId, LicenseGlobals.LicenseServerUrl, GetAttributes());
+        }
+
+        public IDictionary<string, string> GetAttributes()
+        {
+            var id = Preferences.Get(LicenseGlobals.AppId, null);
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new InvalidOperationException("Missing app ID");
+            }
+
+            var attributes = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            attributes.Add(LicenseGlobals.AppId, id);
+            return attributes;
+        }
+    }
+
     public class LicenseService : BaseLicenseService, ILicenseService
     {
         public LicenseService(HttpClient client) : base(client)
