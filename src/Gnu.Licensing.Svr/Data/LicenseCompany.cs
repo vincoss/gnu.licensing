bd6cc16d1f17b9def6e6dec8ec630677ddfdd511
Ferdinand Lukasak <feroalien@hotmail.com>
2020-09-14 08:18:34 +1000

    added company table

---
+++ src/Gnu.Licensing.Svr/Data/LicenseCompany.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gnu.Licensing.Svr.Data
+{
+    public class LicenseCompany
+    {
+        [Key]
+        public int LicenseCompanyId { get; set; }
+
+        [Required]
+        public Guid CompanyUuid { get; set; }
+    }
+}
